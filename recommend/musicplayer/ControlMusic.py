3e65ba0b07f46fa08814c1535962c229cedd4cf1
Electron <devversitymail@gmail.com>
2016-11-15 19:47:45 +0530

    initialized dir structure for project

---
+++ recommend/musicplayer/ControlMusic.py
@@ -0,0 +1,24 @@
+class ControlMusic(object):
+
+    def __init__(self):
+        # don init stuff
+
+    def add(self):
+        # do add songs stuff
+        return False
+
+    def remove(self, songID ):
+        # do remove stuff
+
+        return False
+    
+    def query(self):
+        # do query stuff
+        return {}
+
+    def ManuallyGetRecommendation(self, songID):
+        return {}
+    
+    def ResetAll(self):
+
+        return False
---
+++ recommend/musicplayer/ManageSongs.py
@@ -0,0 +1,28 @@
+class MangaeSongs(object):
+    
+    def TriggerPlaySequence(self, songID):
+        return False
+    
+    def Play(self, songID):
+        return False
+    
+    def playSong(self, songID):
+        return False
+
+    def Pause(self,):
+        return False
+    
+    def next(self, songID):
+        return False
+    
+    def prev(self, songID):
+        return False
+
+    def seek(self, second):
+        return False
+    
+    def Stop(self,):
+        return False
+    
+    def volControl(self, targetVol):
+        return False
