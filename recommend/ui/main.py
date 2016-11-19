1d47d0f9f0f27a6928b15babbe9e820627200e9a
Electron <devversitymail@gmail.com>
2016-11-20 02:35:30 +0530

    WIP - Wireing Player

--- recommend/ui/main.py
+++ recommend/ui/main.py
@@ -1,8 +1,19 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets, QtMultimedia
 import sys
 import os
 import const
 from mainWindow import Ui_MainWindow
+
+filePathList = []
+songsFolder = ""
+# these will be QMediaPlaylist and QMediaPlayer objects, here they are assigned
+# just to silence errors
+mediaPlaylist = []
+mediaPlayer = []
+# user action for play/pause, stop, next, prev
+# -1 is undetermined
+# 0-stopped, 1-playing, 2-paused
+userAction = -1
 
 
 def main():
@@ -28,10 +39,10 @@ def extraSetup(Ui_MainWindow):
     songArtist = window.SongArtist
     songAlbum = window.SongAlbum
     # player Controls
-    prev = window.Prev
-    next = window.Next
-    stop = window.Stop
-    playPause = window.PlayPause
+    prevBtn = window.Prev
+    nextBtn = window.Next
+    stopBtn = window.Stop
+    playPauseBtn = window.PlayPause
     # Volume Controls
     volumeDecr = window.VolumeDecr
     volumeIncr = window.VolumeIncr
@@ -56,25 +67,50 @@ def extraSetup(Ui_MainWindow):
     """
     all events are triggered from here
     """
-
     openFolder.triggered.connect(Browse)
+    # media control handlers
+    playPauseBtn.clicked.connect(playPauseHandler)
+    stopBtn.clicked.connect(stopHandler)
+    prevBtn.clicked.connect(prevHandler)
+    nextBtn.clicked.connect(nextHandler)
 
-    # Create Qt items
-    # mediaPlaylist = QMediaPlaylist()
-    # mediaPlayer = QMediaPlayer()
+    # open webpages
+    aboutProject.triggered.connect(openAboutUrl)
+    seeSourceCode.triggered.connect(openGitHubUrl)
+    reportBug.triggered.connect(openIssuesUrl)
+
+
 """
 Event handler for browse open folder action.
 """
 
+
 def Browse(self):
-    folder = QtWidgets.QFileDialog.getExistingDirectory(None, "Open a folder", os.getenv('HOME'), QtWidgets.QFileDialog.ShowDirsOnly)
-    print(folder)
-    crawlFolder(folder)
-    return folder
+    global songsFolder
+    songsFolder = QtWidgets.QFileDialog.getExistingDirectory(None, "Open a folder", os.getenv('HOME'), QtWidgets.QFileDialog.ShowDirsOnly)
+    print(songsFolder)
+    crawlFolder(songsFolder)
+    return True
+
+
+def openAboutUrl():
+    QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://projectrecommend.github.io/"))
+    return True
+
+
+def openGitHubUrl():
+    QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://github.com/ProjectRecommend/Recommend"))
+    return True
+
+
+def openIssuesUrl():
+    QtGui.QDesktopServices.openUrl(QtCore.QUrl("https://github.com/ProjectRecommend/Recommend/issues"))
+    return True
 
 
 def crawlFolder(folderPath):
-    fileList = []
+    global filePathList
+    filePathList = []
     if folderPath is not None:
         iterator = QtCore.QDirIterator(folderPath, QtCore.QDirIterator.Subdirectories)
         # print(dir(iterator))
@@ -87,10 +123,13 @@ def crawlFolder(folderPath):
                 # print(iterator.fileInfo())
                 if fInfo.suffix() in ('mp3'):
                     # print(fInfo.absoluteFilePath())
-                    fileList.append(fInfo.absoluteFilePath())
-    print(len(fileList))
+                    filePathList.append(fInfo.absoluteFilePath())
+    print(len(filePathList))
     saveSettings()
-    return fileList
+    buildPlayList()
+    buildPlayer()
+    # play()
+    return filePathList
 
 
 def saveSettings():
@@ -101,14 +140,143 @@ def saveSettings():
     settings.setFallbacksEnabled(False)
 
     # check and update music folder location and volume
-    musicFolderLocation = "C:\\Users\\Electron\\Music\\test_music_mp3"
+    global songsFolder
     volume = "30"
-    settings.setValue("musicFolderLocation", musicFolderLocation)
+    settings.setValue("musicFolderLocation", songsFolder)
     settings.setValue("volume", volume)
     print("done")
 
-# def buildPlayList():
-#     pass
+
+def buildPlayList():
+    # Create Qt items
+    global filePathList
+    global mediaPlaylist
+    mediaPlaylist = QtMultimedia.QMediaPlaylist()
+    mediaPlaylist.clear()
+    # self.mediaPlayer = QMediaPlayer()
+    for filepath in filePathList:
+        # print(filepath)
+        mediaPlaylist.addMedia(QtMultimedia.QMediaContent(QtCore.QUrl.fromLocalFile(filepath)))
+    # print(mediaPlaylist.mediaCount())
+
+
+def buildPlayer():
+    global mediaPlaylist
+    global mediaPlayer
+    mediaPlayer = QtMultimedia.QMediaPlayer()
+    mediaPlayer.setPlaylist(mediaPlaylist)
+
+
+# def play():
+#     global mediaPlayer
+#     mediaPlayer.play()
+
+
+def playPauseHandler():
+    global userAction
+    global mediaPlayer
+    global mediaPlaylist
+
+    if not mediaPlaylist:
+        print("playlist is empty add few items")
+
+    # if play list is not empty then proceed
+    if mediaPlaylist:
+        userAction = 1
+        if mediaPlayer.state() == QtMultimedia.QMediaPlayer.StoppedState:
+            if mediaPlayer.mediaStatus() == QtMultimedia.QMediaPlayer.NoMedia:
+                print("no media in playlist")
+                print(mediaPlaylist.mediaCount())
+            elif mediaPlayer.mediaStatus() == QtMultimedia.QMediaPlayer.LoadedMedia:
+                mediaPlayer.play()
+            elif mediaPlayer.mediaStatus() == QtMultimedia.QMediaPlayer.BufferedMedia:
+                mediaPlayer.play()
+        elif mediaPlayer.state() == QtMultimedia.QMediaPlayer.PlayingState:
+            mediaPlayer.pause()
+            userAction = 2
+        elif mediaPlayer.state() == QtMultimedia.QMediaPlayer.PausedState:
+            mediaPlayer.play()
+
+
+def stopHandler():
+    global mediaPlayer
+    global userAction
+    if mediaPlayer.state() == QtMultimedia.QMediaPlayer.PlayingState:
+        mediaPlayer.stop()
+        userAction = 0
+    elif mediaPlayer.state() == QtMultimedia.QMediaPlayer.PausedState:
+        mediaPlayer.stop()
+        userAction = 0
+    elif mediaPlayer.state() == QtMultimedia.QMediaPlayer.StoppedState:
+        pass
+
+
+def prevHandler():
+    global mediaPlayer
+    mediaPlayer.playlist().previous()
+
+
+def nextHandler():
+    global mediaPlayer
+    mediaPlayer.playlist().next()
+
+
+def increaseVolume(self):
+    vol = self.player.volume()
+    vol = min(vol+5,100)
+    self.player.setVolume(vol)
+
+
+def decreaseVolume(self):
+    vol = self.player.volume()
+    vol = max(vol-5,0)
+    self.player.setVolume(vol)
+
+
+def qmp_mediaStatusChanged(self):
+    if self.player.mediaStatus() == QMediaPlayer.LoadedMedia and self.userAction == 1:
+        durationT = self.player.duration()
+        self.centralWidget().layout().itemAt(0).layout().itemAt(1).widget().setRange(0,durationT)
+        self.centralWidget().layout().itemAt(0).layout().itemAt(2).widget().setText('%d:%02d'%(int(durationT/60000),int((durationT/1000)%60)))
+        self.player.play()
+
+
+def qmp_stateChanged(self):
+    if self.player.state() == QMediaPlayer.StoppedState:
+        self.player.stop()
+
+
+def qmp_positionChanged(self, position,senderType=False):
+    sliderLayout = self.centralWidget().layout().itemAt(0).layout()
+    if senderType == False:
+        sliderLayout.itemAt(1).widget().setValue(position)
+    # update the text label
+    sliderLayout.itemAt(0).widget().setText('%d:%02d'%(int(position/60000), int((position/1000)%60)))
+
+
+def seekPosition(self, position):
+    sender = self.sender()
+    if isinstance(sender,QSlider):
+        if self.player.isSeekable():
+            self.player.setPosition(position)
+
+
+def qmp_volumeChanged(self):
+    msg = self.statusBar().currentMessage()
+    msg = msg[:-2] + str(self.player.volume())
+    self.statusBar().showMessage(msg)
+
+
+def increaseVolume(self):
+    vol = self.player.volume()
+    vol = min(vol+5,100)
+    self.player.setVolume(vol)
+
+
+def decreaseVolume(self):
+    vol = self.player.volume()
+    vol = max(vol-5,0)
+    self.player.setVolume(vol)
 
 
 if __name__ == '__main__':
