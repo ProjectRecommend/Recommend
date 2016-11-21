ec04bae2dab857aa9a34c55b04f77abed8ccc044
Raghav Mittal <raghavmittal101@gmail.com>
2016-11-21 16:38:49 +0530

    WIP - returning dictionary of songs

--- recommend/classifier/GetRecommendation.py
+++ recommend/classifier/GetRecommendation.py
@@ -1,3 +1,26 @@
+# ********************************** HARD CODED PART***************************
+import discogs_client as dc
+
+client = dc.Client('test App', user_token="jQgfPZNxlkvrINfWvVjPrtbnfvAQLMrGeJHLyZeO")
+result = client.search(artist = 'Justin Bieber', type='relese', genre='pop', year='2010')
+
+RelevantSongDict = {}        
+# return a dictionary of songs with relevant information
+# make a dictionary out of *result* object
+
+i = 0
+RelevantSongDict['songs'] = {}
+while(i<len(result)):
+    print('*********\n')
+    RelevantSongDict['songs'][i]={}
+    RelevantSongDict['songs'][i]['title'] = result[i].title
+    RelevantSongDict['songs'][i]['videos'] = result[i].videos
+    i = i+1
+print(RelevantSongDict)  
+  
+#**********************************END*****************************************       
+###############################################################################
+#*****************ACTUAL PART**************************************************      
 class GetRecommendation(object):
 
     def __init__(self):
@@ -5,7 +28,28 @@ class GetRecommendation(object):
         pass
 
     def FetchRelevantSong(self, SongMetaData):
-        return {}
+        
+        # This will store dictionary for songs information        
+        RelevantSongDict = {}        
+        
+        # function taken from experiments/webAPI/testdc.py
+        result = client.search(artist = SongMetaData.artist, type='release', genre=SongMetaData.genre, year=SongMetaData.year)
+        
+        # return a dictionary of songs with relevant information
+        # make a dictionary out of *result* object
+        
+        i = 0
+        RelevantSongDict['songs'] = {}
+        
+        # Building the dictionary
+        while(i<len(result)):
+            RelevantSongDict['songs'][i]={}
+            RelevantSongDict['songs'][i]['title'] = result[i].title
+            RelevantSongDict['songs'][i]['videos'] = result[i].videos
+            RelevantSongDict['songs'][i]['url'] = result[i].u
+            i = i+1
+        
+        return {RelevantSongDict}
 
     def Predict(self, SongMetaData, RelevantSongDict):
         return {}
