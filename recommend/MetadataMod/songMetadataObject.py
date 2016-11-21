86a8902f44c7e63d2c98cf6812bbbaba779d26b0
Electron <devversitymail@gmail.com>
2016-11-21 16:45:25 +0530

    added songmetadataobject

--- recommend/MetadataMod/songMetadataObject.py
+++ recommend/MetadataMod/songMetadataObject.py
@@ -1,4 +1,17 @@
+# SongMetadata Object, it is used to pass data(ID3 data) around
+# see this : https://en.wikipedia.org/wiki/ID3
+# for more info about ID3 tags
+
+
 class SongMetadata(object):
     def __init__(self):
-        # super(SongMetadata, selinit_*args))
-        
+        self.artistNameTPE1 = None
+        self.artistNameTPE2 = None
+        self.albumNameTALB = None
+        self.artistNameTSOP = None
+        self.lyricsUSLT = None
+        self.dateAndYearOfRecordingTDRC = None
+        self.dateAndYearOfReleaseTDOR = None
+        self.publisherTPUB = None
+        self.titleTIT2 = None
+        self.genreTCON = None
