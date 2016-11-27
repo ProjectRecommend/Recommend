eded8265f941d94db4b3edc134eab043696882de
Shakthi <shakthivvbhss@gmail.com>
2016-11-27 19:22:26 +0530

    WIP -test_GUI.py

---
+++ tests/test_GUI.py
@@ -0,0 +1,32 @@
+import sys
+import unittest
+import PyQt5
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtTest import QTest
+from PyQt5.QtCore import Qt
+sys.path.append('../recommend/')
+from main import MainWindow as mnw
+app = QtWidgets.QApplication(sys.argv)
+
+class Test1_extraSetup(unittest.TestCase):
+    '''test recommend GUI'''
+    def setUp(self):
+        '''testing initializes'''
+        global obj
+        obj = mnw() # 'obj' an object of class 'mnw'
+
+    def test_browseHandler(self):
+        self.assertTrue(obj.browseHandler(self))
+        #self.assertEqual(editMet)
+
+    def tearDown(self):
+        del globals()['obj']
+
+#class
+
+    '''def test_default(self):
+
+        self.assertEqual(VolumeIncr.setEnabled,True)
+        #self.assertEqual(self.VolumeDecr.setEnabled,True)
+        #self.assertEqual(self.Next.setDefault,False)
+    '''
