f698acf6d7f9a6ce27bdc2dc593200cc34cb08c2
Electron <devversitymail@gmail.com>
2016-11-29 06:17:22 +0530

    fix style in tests

--- tests/test_localStorage.py
+++ tests/test_localStorage.py
@@ -7,33 +7,44 @@ from LocalStorage.ManageLocalStorageModule import ManageLocalStorage as mls
 from LocalStorage.AccessLocalStorageModule import AccessLocalStorage as als
 
 # TESTS for ManageLocalStorageModule
-## CLASS ManageLocalStorage
+# CLASS ManageLocalStorage
 
-#### tests for connect() and disconnect() are not written becaues they are not in use
+# tests for connect() and disconnect() are not written becaues they are not in use
 
-### test for 'build'
+# test for 'build'
+
+
 class Test10_build(unittest.TestCase):
     def setUp(self):
         global obj_mls
         obj_mls = mls('someConnect123')
+
     def test_build(self):
         self.assertEqual(obj_mls.build(), True)
 
-### test for 'query'
+# test for 'query'
+
+
 class Test20_query(unittest.TestCase):
     def setUp(self):
         pass
+
     def test_query(self):
         self.assertNotEqual(obj_mls.query(), "Query failed")
+
     def tearDown(self):
         pass
 
-### test for 'dump'
+# test for 'dump'
+
+
 class Test50_dump(unittest.TestCase):
     def setUp(self):
         pass
+
     def test_dump(self):
         self.assertEqual(obj_mls.dump(), True)
+
     def tearDown(self):
         del globals()['obj_mls']
         del globals()['obj_als']
